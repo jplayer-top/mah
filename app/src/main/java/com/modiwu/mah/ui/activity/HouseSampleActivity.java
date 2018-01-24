5667abc7d16a07e969648654a2e147b040d4161a
oblivion0001 <18366108542@163.com>
2018-01-24 18:03:47 +0800

    obl-company

--- app/src/main/java/com/modiwu/mah/ui/activity/HouseSampleActivity.java
+++ app/src/main/java/com/modiwu/mah/ui/activity/HouseSampleActivity.java
@@ -1,7 +1,11 @@
 package com.modiwu.mah.ui.activity;
 
+import android.view.View;
+
 import com.modiwu.mah.R;
 import com.modiwu.mah.base.BaseCommonActivity;
+
+import top.jplayer.baseprolibrary.widgets.DialogFlowSure;
 
 /**
  * Created by Obl on 2018/1/24.
@@ -17,5 +21,14 @@ public class HouseSampleActivity extends BaseCommonActivity {
     @Override
     public void initBaseData() {
         tvBarTitle.setText("样板间征集");
+        final DialogFlowSure rxDialogSure = new DialogFlowSure(mBaseActivity);//提示弹窗
+        rxDialogSure.getLogoView().setImageResource(R.mipmap.ic_launcher);
+        rxDialogSure.getSureView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rxDialogSure.cancel();
+            }
+        });
+        rxDialogSure.show();
     }
 }
---
+++ baseprolibrary/src/main/java/top/jplayer/baseprolibrary/widgets/DialogFlowSure.java
@@ -0,0 +1,93 @@
+package top.jplayer.baseprolibrary.widgets;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import top.jplayer.baseprolibrary.R;
+
+
+/**
+ * Created by vondear on 2016/7/19.
+ * Mainly used for confirmation and cancel.
+ */
+public class DialogFlowSure extends BaseCommonDialog {
+
+    private ImageView mIvLogo;
+    private TextView mTvTitle;
+    private TextView mTvContent;
+    private TextView mTvSure;
+
+    public DialogFlowSure(Context context, int themeResId) {
+        super(context, themeResId);
+        initView();
+    }
+
+    public DialogFlowSure(Context context, boolean cancelable, DialogInterface.OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+        initView();
+    }
+
+    public DialogFlowSure(Context context) {
+        super(context);
+        initView();
+    }
+
+    public DialogFlowSure(Context context, float alpha, int gravity) {
+        super(context, alpha, gravity);
+        initView();
+    }
+
+    public ImageView getLogoView() {
+        return mIvLogo;
+    }
+
+    public TextView getTitleView() {
+        return mTvTitle;
+    }
+
+    public TextView getSureView() {
+        return mTvSure;
+    }
+
+    public void setSureListener(View.OnClickListener listener) {
+        mTvSure.setOnClickListener(listener);
+    }
+
+    public TextView getContentView() {
+        return mTvContent;
+    }
+
+    public void setLogo(int resId) {
+        mIvLogo.setImageResource(resId);
+    }
+
+    public void setTitle(String title) {
+        mTvTitle.setText(title);
+    }
+
+    public void setSure(String content) {
+        mTvSure.setText(content);
+    }
+
+    public void setContent(String str) {
+        mTvContent.setText(str);
+    }
+
+    private void initView() {
+        View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_sure, null);
+        mTvSure = (TextView) dialogView.findViewById(R.id.tv_sure);
+        mTvTitle = (TextView) dialogView.findViewById(R.id.tv_title);
+        mTvTitle.setTextIsSelectable(true);
+        mTvContent = (TextView) dialogView.findViewById(R.id.tv_content);
+        mTvContent.setMovementMethod(ScrollingMovementMethod.getInstance());
+        mTvContent.setTextIsSelectable(true);
+        mIvLogo = (ImageView) dialogView.findViewById(R.id.iv_logo);
+        setContentView(dialogView);
+    }
+
+}
