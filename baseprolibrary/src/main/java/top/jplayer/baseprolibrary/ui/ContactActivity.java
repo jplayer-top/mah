6d8f38ac1f6735a212d31b5232c3c769fde1e06f
oblivion0001 <18366108542@163.com>
2018-01-31 18:09:52 +0800

    obl-company

--- baseprolibrary/src/main/java/top/jplayer/baseprolibrary/ui/ContactActivity.java
+++ baseprolibrary/src/main/java/top/jplayer/baseprolibrary/ui/ContactActivity.java
@@ -1,5 +1,6 @@
 package top.jplayer.baseprolibrary.ui;
 
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -13,6 +14,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import top.jplayer.baseprolibrary.R;
 import top.jplayer.baseprolibrary.mvp.model.bean.ModelContactCity;
 import top.jplayer.baseprolibrary.ui.adapter.ContactCityAdapter;
@@ -20,7 +24,9 @@ import top.jplayer.baseprolibrary.utils.ComparatorLetter;
 import top.jplayer.baseprolibrary.widgets.sidebar.PinnedHeaderDecoration;
 import top.jplayer.baseprolibrary.widgets.sidebar.WaveSideBarView;
 
-
+/**
+ * 城市（联系人）列表
+ */
 public class ContactActivity extends SuperBaseActivity {
 
     RecyclerView mRecyclerView;
@@ -38,46 +44,33 @@ public class ContactActivity extends SuperBaseActivity {
         mSideBarView = contentView.findViewById(R.id.side_view);
         mRecyclerView = contentView.findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         final PinnedHeaderDecoration decoration = new PinnedHeaderDecoration();
-        decoration.registerTypePinnedHeader(1, new PinnedHeaderDecoration.PinnedHeaderCreator() {
-            @Override
-            public boolean create(RecyclerView parent, int adapterPosition) {
-                return true;
-            }
-        });
+        decoration.registerTypePinnedHeader(1, (parent, adapterPosition) -> true);
         mRecyclerView.addItemDecoration(decoration);
 
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Type listType = new TypeToken<ArrayList<ModelContactCity>>() {
-                }.getType();
-                Gson gson = new Gson();
-                final List<ModelContactCity> list = gson.fromJson(ModelContactCity.DATA, listType);
-                Collections.sort(list, new ComparatorLetter());
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mAdapterContactCity = new ContactCityAdapter(list);
-                        mRecyclerView.setAdapter(mAdapterContactCity);
-                    }
+        Observable.just(1).subscribeOn(Schedulers.io())
+                .map(o -> {
+                    Type listType = new TypeToken<ArrayList<ModelContactCity>>() {
+                    }.getType();
+                    Gson gson = new Gson();
+                    final List<ModelContactCity> list = gson.fromJson(ModelContactCity.DATA, listType);
+                    Collections.sort(list, new ComparatorLetter());
+                    return list;
+                }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(modelContactCities -> {
+                    mAdapterContactCity = new ContactCityAdapter(modelContactCities);
+                    mRecyclerView.setAdapter(mAdapterContactCity);
                 });
-            }
-        }).start();
 
-        mSideBarView.setOnTouchLetterChangeListener(new WaveSideBarView.OnTouchLetterChangeListener() {
-            @Override
-            public void onLetterChange(String letter) {
-                int pos = mAdapterContactCity.getLetterPosition(letter);
+        mSideBarView.setOnTouchLetterChangeListener(letter -> {
+            int pos = mAdapterContactCity.getLetterPosition(letter);
 
-                if (pos != -1) {
-                    mRecyclerView.scrollToPosition(pos);
-                    LinearLayoutManager mLayoutManager =
-                            (LinearLayoutManager) mRecyclerView.getLayoutManager();
-                    mLayoutManager.scrollToPositionWithOffset(pos, 0);
-                }
+            if (pos != -1) {
+                mRecyclerView.scrollToPosition(pos);
+                LinearLayoutManager mLayoutManager =
+                        (LinearLayoutManager) mRecyclerView.getLayoutManager();
+                mLayoutManager.scrollToPositionWithOffset(pos, 0);
             }
         });
     }
